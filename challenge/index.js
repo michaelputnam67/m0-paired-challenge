ca9d6740317231326b9905eb6e2bef87098bbaf0
Michael Putnam <mikeputnam713@gmail.com>
2022-01-12 19:17:11 -0800

    Add Book Index

---
+++ challenge/index.js
@@ -0,0 +1,7 @@
+var haveRead = true
+var numberOfTitlesInSeries = 14
+var magicUser = "Rand"
+var rogue = "Matt"
+var authorLiving = true
+var avgPageLength = 650
+var characters = ["rand", 'matt', 'perrin', 'elayne'];
